6b34834043c8dac606ef6497203cc205e148e09a
Chris Olivier <cjolivier01@gmail.com>
2019-11-11 17:16:16 -0800

    Cherry pick xla fixes from tf13

--- tensorflow/tools/xla_extract/tf_graph_to_xla_lib.cc
+++ tensorflow/tools/xla_extract/tf_graph_to_xla_lib.cc
@@ -1,10 +1,12 @@
 #include "tensorflow/tools/xla_extract/tf_graph_to_xla_lib.h"
+#include <google/protobuf/util/json_util.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <algorithm>
 #include <iterator>
 #include <string>
 #include <tuple>
+#include <utility>
 #include "tensorflow/compiler/tf2xla/xla_compiler.h"
 #include "tensorflow/compiler/tf2xla/xla_op_registry.h"  // for DEVICE_CPU_XLA_JIT
 #include "tensorflow/compiler/xla/client/client_library.h"
@@ -17,10 +19,13 @@
 #include "tensorflow/compiler/xla/service/hlo_proto_util.h"
 #include "tensorflow/compiler/xla/service/interpreter/compiler.h"
 
-#include <utility>
+#include "tensorflow/core/protobuf/config.pb.h"
+#include "tensorflow/core/protobuf/rewriter_config.pb.h"
+
 #include "tensorflow/compiler/xla/service/algebraic_simplifier.h"
 #include "tensorflow/compiler/xla/service/call_inliner.h"
 #include "tensorflow/compiler/xla/service/despecializer.h"
+#include "tensorflow/compiler/xla/service/platform_util.h"
 #include "tensorflow/compiler/xla/service/flatten_call_graph.h"
 #include "tensorflow/compiler/xla/service/hlo_constant_folding.h"
 #include "tensorflow/compiler/xla/service/hlo_cse.h"
@@ -33,27 +38,127 @@
 #include "tensorflow/core/lib/strings/str_util.h"
 namespace tensorflow {
 
+namespace {  // anonymous namepace
+
+bool is_true(const char *s) {
+    if (s && *s) {
+        const char c = ::tolower(*s);
+        if (c == 'y' || c == 't') {
+            return true;
+        }
+        return atoi(s) > 0;
+    }
+    return false;
+}
+
+bool get_env_bool(const char *s, const bool dflt) {
+    const char *v = getenv(s);
+    if (v && *v) {
+      return is_true(v);
+    }
+    return dflt;
+}
+
+int get_env_int(const char *s, const int dflt) {
+  const char* v = getenv(s);
+  if (v && *v) {
+    return atoi(v);
+  }
+  return dflt;
+}
+
+const bool save_messages = get_env_bool("XLA_SAVE_MESSAGES", false);
+const bool verbose = get_env_int("XLA_LOG", NO_LOG) >= DEBUG_LOG;
+
+template <typename MSG>
+std::string msg_to_json(const MSG& msg) {
+  std::string json;
+  google::protobuf::util::JsonPrintOptions op;
+  op.add_whitespace = true;
+  google::protobuf::util::MessageToJsonString(msg, &json, op);
+  return std::move(json);
+}
+
+template <typename MSG>
+bool save_msg(const MSG& msg, const std::string& file) {
+  const std::string json = msg_to_json(msg);
+
+  FILE* f = fopen(file.c_str(), "wt");
+  if (f) {
+    fwrite(json.c_str(), json.size(), sizeof(std::string::value_type), f);
+    fclose(f);
+    return true;
+  } else {
+    LOG(ERROR) << "Could not open file: " << file
+               << ", reason: " << strerror(errno) << std::endl
+               << std::flush;
+    return false;
+  }
+}
+
 std::vector<XlaCompiler::Argument> BuildXlaArgsFromClientGraph(
     const std::unique_ptr<ClientGraph>& cg) {
   std::vector<XlaCompiler::Argument> xla_args;
   for (const Node* node : cg->graph.nodes()) {
+    if (verbose) {
+        LOG(INFO) << "Inspecting node " << node->name() 
+                  << " of type: " << node->type_string() 
+                  << std::endl;
+    }
     if (node->type_string() == "XlaLaunch") {
       // iterate over the inputs to this node for the args
       for (const Node* in : node->in_nodes()) {
-        auto in_def = in->def();
+        const NodeDef& in_def = in->def();
         XlaCompiler::Argument arg;
-        if (in_def.op() == "VarHandleOp") {
+        const std::string op_name = in_def.op();
+        if (verbose) {
+            const std::string node_name = in_def.name();
+            LOG(INFO) << "Node: " << node_name << ", Op: " << op_name
+                      << ", type: " << in->type_string()
+                      << ", req device: " << in->requested_device() << std::endl
+                      << std::flush;
+        }
+        if (op_name == "VarHandleOp") {
           arg.kind = XlaCompiler::Argument::kResource;
           arg.resource_kind = XlaResource::kVariable;
           arg.initialized = true;
           tensorflow::TensorShape shape_value;
-          GetNodeAttr(in_def, "shape", &shape_value);
-          arg.shape = shape_value;
+          Status status = GetNodeAttr(in_def, "shape", &shape_value);
+          if (!status.ok()) {
+            LOG(WARNING) << status.error_message() << ", code = " << status.code()
+                         << std::endl;
+          } else {
+            arg.shape = shape_value;
+          }
         } else {
+          if (verbose) {
+            const std::string node_json = msg_to_json(in_def);
+            printf("\n%s\n", node_json.c_str());  fflush(stdout);
+          }
+
           arg.kind = XlaCompiler::Argument::kParameter;
           std::vector<tensorflow::TensorShape> shape_value;
-          GetNodeAttr(in_def, "_output_shapes", &shape_value);
-          arg.shape = shape_value[0];
+          Status status = GetNodeAttr(in_def, "_output_shapes", &shape_value);
+          if (!status.ok()) {
+            LOG(WARNING) << status.error_message()
+                        << ", code = " << status.code() << std::endl;
+          }
+          if (verbose) {
+            LOG(INFO) << "_output_shapes: shape_value.size() = "
+                        << shape_value.size() << " (" << status.error_message()
+                        << ")" << std::endl;
+          }
+          if (status.ok()) {
+              assert(!shape_value.empty());
+              arg.shape = shape_value[0];
+          } else {
+            // fall back to 'shape' if there was no '_output_shapes'
+            status = GetNodeAttr(in_def, "shape", &(arg.shape));
+            if (!status.ok()) {
+              LOG(ERROR) << status.error_message()
+                         << ", code = " << status.code() << std::endl;
+            }
+          }
         }
         arg.name = in_def.name();
 
@@ -67,22 +172,69 @@ std::vector<XlaCompiler::Argument> BuildXlaArgsFromClientGraph(
   }
   return std::move(xla_args);
 }
+
 void InitializeDevices(const SessionOptions& options, DeviceMgr** device_mgr,
-                       DeviceSet* dev_set) {
+                              DeviceSet* dev_set) {
   std::vector<std::unique_ptr<Device>> devices;
-  Status s = DeviceFactory::AddDevices(
-      options, "/job:localhost/replica:0/task:0", &devices);
+  Status s = DeviceFactory::AddDevices(options, "/job:localhost/replica:0/task:0", &devices);
   *device_mgr = new DeviceMgr(std::move(devices));
+  bool have_device = false;
   int devices_added = 0;
-  for (auto d : (*device_mgr)->ListDevices()) {
-    dev_set->AddDevice(d);
-    d->op_segment()->AddHold("HOLD");
-    if (devices_added == 0) {
-      dev_set->set_client_device(d);
+  for (Device *d : (*device_mgr)->ListDevices()) {
+    const std::string device_type = d->device_type();
+    if (verbose) {
+      LOG(INFO) << "Found Device: " << d->name() << " (" << device_type << ")"
+                << std::endl
+                << std::flush;
     }
-    ++devices_added;
+    // GPU devices alter the client graph in an incompatible way to the curent implementation
+    if (device_type == DEVICE_XLA_CPU || device_type == "CPU") {
+      dev_set->AddDevice(d);
+      d->op_segment()->AddHold("HOLD");
+      const std::string& device_name = d->name();
+      if (!have_device) {
+        if (device_type == "CPU") {
+          if (verbose) {
+            LOG(INFO) << "Setting client device to: " << device_name << std::endl
+                      << std::flush;
+          }
+          dev_set->set_client_device(d);
+          have_device = true;
+        }
+        ++devices_added;
+      }
+    }
+  }
+  if (!have_device) {
+      throw std::runtime_error("Did not find a suitable client device");
+  }
+  if (verbose) {
+    LOG(INFO) << "Added " << devices_added << " devices" << std::endl << std::flush;
   }
 }
+
+/**
+ * @brief Get the Compile Platform object (i.e. "Host", "CUDA", etc.)
+ * 
+ * @return se::Platform* Pointer to Platform object to use for compile (prefer "Host")
+ */
+se::Platform* getCompilePlatform() {
+    xla::StatusOr<std::vector<se::Platform*>> sop = xla::PlatformUtil::GetSupportedPlatforms();
+    std::vector<se::Platform*>& platforms = sop.ValueOrDie();
+    se::Platform *platform = nullptr;
+    for (se::Platform *p : platforms) {
+        if (verbose) {
+            LOG(INFO) << "Found platform: " << p->Name() << std::endl;
+        }
+        // Get first one, or take "Host" if found
+        if (!platform || p->Name() == "Host") {
+            platform = p;
+        }
+    }
+    return platform;
+};
+
+}  // anonymous namespace
 
 xla::HloModuleProto ExtractHloFromGraphDef(const GraphDef& in_graph,
                                            const std::string& fetch) {
@@ -100,24 +252,31 @@ xla::HloModuleProto ExtractHloFromGraphDef(const GraphDef& in_graph,
   std::unique_ptr<GraphExecutionState> execution_state;
   s = GraphExecutionState::MakeForBaseGraph(&gdef, ges_options,
                                             &execution_state);
-  if (!s.ok())
+  if (!s.ok()) {
     LOG(ERROR) << "execution state creation failed: " << s.error_message();
+  }
   BuildGraphOptions bg_options;
   bg_options.use_function_convention = true;
   std::istringstream fetch_stream(fetch);
   std::vector<std::string> fetches(
       std::istream_iterator<std::string>{fetch_stream},
       std::istream_iterator<std::string>());
-  for (std::string fetch0 : fetches) {
-    bg_options.callable_options.add_fetch(fetch0);
+  for (const std::string& fetch : fetches) {
+    bg_options.callable_options.add_fetch(fetch);
   }
   std::unique_ptr<ClientGraph> client_graph;
   s = execution_state->BuildGraph(bg_options, &client_graph);
-  if (!s.ok()) LOG(ERROR) << "build graph failed " << s.error_message();
+  if (!s.ok()) {
+      LOG(ERROR) << "build graph failed " << s.error_message();
+  }
 
   // Usually there is only one cluster, but for some graphs (e.g. LSTM) there
   // may be more.  Return the *last* cluster whose name starts with "cluster_"
   FunctionDefLibrary fdef_lib = client_graph->flib_def->ToProto();
+
+  if (save_messages) {
+    save_msg(fdef_lib , "FunctionDefLibrary.json");
+  }
 
   auto fdef_iter =
       std::find_if(fdef_lib.function().rbegin(), fdef_lib.function().rend(),
@@ -137,23 +296,32 @@ xla::HloModuleProto ExtractHloFromGraphDef(const GraphDef& in_graph,
                      });
   }
 
-  if (fdef_iter != fdef_lib.function().rend()) {
-    fdef = *fdef_iter;
-  } else {
-    fdef = *fdef_lib.function().begin();
-    LOG(INFO) << "cluster not found, using " << fdef.signature().name()
-              << " instead\n";
+  if (fdef_iter == fdef_lib.mutable_function()->rend()) {
+    fdef_iter = fdef_lib.mutable_function()->rend()-1;
+    FunctionDef temp_fdef = *fdef_iter;
+ 
+    if(verbose) {
+      LOG(INFO) << "cluster not found, using " << temp_fdef.signature().name()
+                << " instead\n";
+    }
   }
 
-  auto xla_args = BuildXlaArgsFromClientGraph(client_graph);
+  FunctionDef& fdef = *fdef_iter;
+
+  if (save_messages) {
+    save_msg(fdef, "fdef.json");
+  }
+
+  std::vector<XlaCompiler::Argument> xla_args = BuildXlaArgsFromClientGraph(client_graph);
 
   // to make sure xla_args matches fdef
-
-  LOG(INFO) << "number of function defs:" << fdef_lib.function().size() << "\n";
-  LOG(INFO) << fdef.signature().name() << "\n";
-  LOG(INFO) << "xla args number:" << xla_args.size() << "\n";
-  LOG(INFO) << "fdef_args number:" << fdef.signature().input_arg().size()
-            << "\n";
+  if(verbose) {
+    LOG(INFO) << "number of function defs:" << fdef_lib.function().size() << "\n";
+    LOG(INFO) << fdef.signature().name() << "\n";
+    LOG(INFO) << "xla args number:" << xla_args.size() << "\n";
+    LOG(INFO) << "fdef_args number:" << fdef.signature().input_arg().size()
+              << "\n";
+  }
 
   // compares fdef_args(ground truth) with xla_args
   // prunes away extra args and reorders to match fdef_args
@@ -216,12 +384,23 @@ xla::HloModuleProto ExtractHloFromGraphDef(const GraphDef& in_graph,
   xla_args = new_xla_args;
   // we no longer need to do the rotation
 
-  LOG(INFO) << "xla args in correct order and matches fdef\n";
+  if (verbose) {
+    LOG(INFO) << "xla args in correct order and matches fdef\n";
+  }
   xla::HloModuleProto hmod;
   {
     DeviceType device_type(DEVICE_CPU_XLA_JIT);
     XlaCompiler::Options compile_options;
-    compile_options.client = xla::ClientLibrary::LocalClientOrDie();
+
+    se::Platform *platform = getCompilePlatform();
+    if (!platform) {
+        throw std::runtime_error("Could not determine platform for compile");
+    }
+    if (verbose) {
+        LOG(INFO) << "Using platform: " << platform->Name() << std::endl << std::flush;
+    }
+    auto soc = xla::ClientLibrary::GetOrCreateCompileOnlyClient(platform);
+    compile_options.client = soc.ValueOrDie();
     compile_options.device_type = device_type;
     compile_options.flib_def = client_graph->flib_def.get();
 
@@ -236,7 +415,9 @@ xla::HloModuleProto ExtractHloFromGraphDef(const GraphDef& in_graph,
                                  xla_args, &result);
 
     if (!s.ok()) LOG(ERROR) << "Couldn't compile to xla: " << s.error_message();
-    LOG(INFO) << "Done Compiling";
+    if (verbose) {
+      LOG(INFO) << "Done Compiling";
+    }
     hmod.CopyFrom(result.computation->proto());
 
     // hlo optimizations
@@ -275,10 +456,13 @@ xla::HloModuleProto ExtractHloFromGraphDef(const GraphDef& in_graph,
     // hlo optimization run
     s = pipeline.Run(hlo_module.get()).status();
 
-    if (!s.ok())
+    if (!s.ok()) {
       LOG(ERROR) << "Couldn't Run HloOptimization: " << s.error_message();
+    }
 
-    LOG(INFO) << "Done HLO Optimization\n";
+    if (verbose) {
+      LOG(INFO) << "Done HLO Optimization\n";
+    }
     hmod = hlo_module.get()->ToProto();
 
     auto* comps = hmod.mutable_computations();
@@ -312,12 +496,22 @@ xla::HloModuleProto ExtractHloFromGraphDef(const GraphDef& in_graph,
 }
 
 Status xla_extract_via_strings(const std::string& graph_def_msg,
-                               const std::string& target_node,
-                               std::string* out_graph) {
+                                const std::string& target_node,
+                                std::string* out_graph) {
   GraphDef gdef;
+
   gdef.ParseFromString(graph_def_msg);
+
+  if (save_messages) {
+    save_msg(gdef, "graph.json");
+  }
+
   auto hmod = ExtractHloFromGraphDef(gdef, target_node);
   hmod.SerializeToString(out_graph);
+
+  if(get_env_int("XLA_LOG", NO_LOG) >= INFO_LOG) {
+      std::cout << "XLA Extraction Complete\n";
+  }
 
   return Status::OK();
 }
