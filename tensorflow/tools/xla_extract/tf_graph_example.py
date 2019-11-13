c9789aacb898cdc81ae9b46e62312dece05858d1
Vishal Subbiah <vishal@cerebras.net>
2019-11-13 13:13:27 -0800

    updating test script

--- tensorflow/tools/xla_extract/tf_graph_example.py
+++ tensorflow/tools/xla_extract/tf_graph_example.py
@@ -23,42 +23,41 @@ def model_fn(features, labels, mode=tf.estimator.ModeKeys.TRAIN, params=None):
     data_format = "channels_first"
     #jit_scope = tf.python.compiler.jit.experimental_jit_scope
     with tf.variable_scope("eg_model", use_resource=True):
-        with tf.device("/job:localhost/replica:0/task:0/device:XLA_CPU:0"):
-            conv1 = keras.layers.Conv2D(
-                filters=4,
-                kernel_size=[3, 3],
-                padding="same",
-                activation=tf.nn.relu,
-                data_format=data_format)(features)
-            pool1 = keras.layers.MaxPooling2D(
-                pool_size=(2, 2),
-                data_format=data_format)(conv1)
-            conv2 = keras.layers.Conv2D(
-                filters=4,
-                kernel_size=[3, 3],
-                padding="same",
-                activation=tf.nn.relu,
-                data_format=data_format)(pool1)
+        conv1 = keras.layers.Conv2D(
+            filters=4,
+            kernel_size=[3, 3],
+            padding="same",
+            activation=tf.nn.relu,
+            data_format=data_format)(features)
+        pool1 = keras.layers.MaxPooling2D(
+            pool_size=(2, 2),
+            data_format=data_format)(conv1)
+        conv2 = keras.layers.Conv2D(
+            filters=4,
+            kernel_size=[3, 3],
+            padding="same",
+            activation=tf.nn.relu,
+            data_format=data_format)(pool1)
 
-            flat = keras.layers.Flatten(
-                data_format=data_format)(conv2)
-            logits = keras.layers.Dense(
-                units=num_classes,
-                use_bias=False)(flat)
+        flat = keras.layers.Flatten(
+            data_format=data_format)(conv2)
+        logits = keras.layers.Dense(
+            units=num_classes,
+            use_bias=False)(flat)
 
-            labels = tf.one_hot(labels, depth=num_classes)
-            loss = tf.reduce_mean(
-                tf.nn.softmax_cross_entropy_with_logits_v2(
-                    labels=labels, logits=logits), name='loss')
+        labels = tf.one_hot(labels, depth=num_classes)
+        loss = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits_v2(
+                labels=labels, logits=logits), name='loss')
 
-            learning_rate = 0.1
-            train_op = \
-                tf.train.GradientDescentOptimizer(
-                    learning_rate, name="train_step").minimize(
-                    loss, global_step=tf.train.get_global_step())
+        learning_rate = 0.1
+        train_op = \
+            tf.train.GradientDescentOptimizer(
+                learning_rate, name="train_step").minimize(
+                loss, global_step=tf.train.get_global_step())
 
-            with ops.control_dependencies([train_op]):
-                return array_ops.identity(loss, name=loss.op.name)
+        with ops.control_dependencies([train_op]):
+            return array_ops.identity(loss, name=loss.op.name)
 
 xshape, yshape = [16, 3, 32, 32], [16, 1]
 
@@ -81,5 +80,5 @@ from tensorflow.tools.xla_extract import XlaExtract
 print(loss)
 hlo_mod = XlaExtract(loss)
 
-with open("xla_outtest.pbtxt", 'w') as f:
+with open("xla_out.pbtxt", 'w') as f:
     f.write(str(hlo_mod))
