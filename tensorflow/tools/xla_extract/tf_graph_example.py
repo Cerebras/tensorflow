7f792731f98ac521e4abafb98f7f1f2d3f4463a1
cjolivier01 <cjolivier01@gmail.com>
2019-12-05 22:23:13 -0800

    ..

--- tensorflow/tools/xla_extract/tf_graph_example.py
+++ tensorflow/tools/xla_extract/tf_graph_example.py
@@ -4,14 +4,23 @@ This provides a simple Conv-Pool-Conv-FC tensorflow model
 for testing xla extraction
 """
 
+import os
+
+#os.environ["TF_XLA_FLAGS"] = "--xla_dump_executions_to = xla_out.comp"
+#os.environ["XLA_FLAGS"] = "--xla_dump_hlo_as_text --xla_dump_to=./hlo"
+os.environ["XLA_FLAGS"] = "--xla_dump_hlo_as_text --xla_dump_to=./hlo"
+
+
 import tensorflow as tf
 import logging
 import numpy as np
+import shutil
 from tensorflow.python import keras
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
 from tensorflow.contrib.compiler.xla import compile
 
+_WITH_SUMMARIES = False
 
 def model_fn(features, labels, mode=tf.estimator.ModeKeys.TRAIN, params=None):
     ''' This function is the input to Estimator constructor.
@@ -31,12 +40,19 @@ def model_fn(features, labels, mode=tf.estimator.ModeKeys.TRAIN, params=None):
         pool1 = keras.layers.MaxPooling2D(
             pool_size=(2, 2),
             data_format=data_format)(conv1)
+
+        if _WITH_SUMMARIES:
+            tf.summary.scalar('summary_pool1_max', tf.math.reduce_max(pool1))
+
         conv2 = keras.layers.Conv2D(
             filters=4,
             kernel_size=[3, 3],
             padding="same",
             activation=tf.nn.relu,
             data_format=data_format)(pool1)
+
+        if _WITH_SUMMARIES:
+            tf.summary.image('summary_conv2_image', conv2)
 
         flat = keras.layers.Flatten(
             data_format=data_format)(conv2)
@@ -48,6 +64,9 @@ def model_fn(features, labels, mode=tf.estimator.ModeKeys.TRAIN, params=None):
         loss = tf.reduce_mean(
             tf.nn.softmax_cross_entropy_with_logits_v2(
                 labels=labels, logits=logits), name='loss')
+
+        if _WITH_SUMMARIES:
+            tf.summary.scalar('summary_mean_loss', tf.math.reduce_mean(loss))
 
         learning_rate = 0.1
         train_op = \
@@ -79,5 +98,25 @@ from tensorflow.tools.xla_extract import XlaExtract
 
 hlo_mod = XlaExtract(loss)
 
-with open("xla_out.pbtxt", 'w') as f:
+hlo_mod_string = hlo_mod.SerializeToString()
+
+with open("xla_out.proto", 'w') as f:
     f.write(str(hlo_mod))
+
+with open("xla_out.bin", 'w') as f:
+    f.write(str(hlo_mod_string))
+
+def _draw_graph(logdir):
+    with tf.Session() as sess:
+        writer = tf.summary.FileWriter(logdir, sess.graph)
+
+# XLA dump: https://groups.google.com/forum/#!topic/xla-dev/nMzNZfk-Jhw
+
+TB_LOGDIR = "/tmp/tblogdir"
+if os.path.exists(TB_LOGDIR):
+    shutil.rmtree(TB_LOGDIR)
+    os.mkdir(TB_LOGDIR, mode=0o777)
+
+_draw_graph(TB_LOGDIR)
+
+
