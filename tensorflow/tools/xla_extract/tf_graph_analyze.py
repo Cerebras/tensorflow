6ffbdf18f4287a42c02d7cbebb43918606618159
Chris Olivier <cjolivier01@gmail.com>
2019-12-06 17:29:27 -0800

    added

--- tensorflow/tools/xla_extract/tf_graph_analyze.py
+++ tensorflow/tools/xla_extract/tf_graph_analyze.py
@@ -17,10 +17,22 @@ import tensorflow as tf
 import logging
 import numpy as np
 import shutil
+import pathlib
+import graphviz
+import re
+import pydotplus
+
+from pydotplus import graphviz as pydot
+
+from os import listdir
+from os.path import isfile, join
+
+from graphviz import Source
 from tensorflow.python import keras
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
 from tensorflow.contrib.compiler.xla import compile
+from tensorflow.python.client import timeline
 
 _WITH_SUMMARIES = False
 
@@ -114,59 +126,96 @@ def _standard_compile(model_fn, features, labels):
       # chrome://tracing/.
       if i == train_loops - 1:
         sess.run(loss,
-                 feed_dict={x: batch_xs,
-                            y_: batch_ys},
+                 feed_dict={features: batch_xs,
+                            labels: batch_ys},
                  options=tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE),
                  run_metadata=run_metadata)
         trace = timeline.Timeline(step_stats=run_metadata.step_stats)
         with open('timeline.ctf.json', 'w') as trace_file:
           trace_file.write(trace.generate_chrome_trace_format())
       else:
-        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+        sess.run(loss, feed_dict={features: batch_xs, labels: batch_ys})
+
+    # def render(self, filename=None, directory=None, view=False, cleanup=False,
+    #            format=None, renderer=None, formatter=None,
+    #            quiet=False, quiet_view=False):
+
+
+def _show_files(path, start=0, end=None):
+    all_files = [f for f in listdir(path) if isfile(join(path, f))]
+    dot_files = [f for f in all_files if pathlib.Path(join(path, f)).suffix == ".dot"]
+
+    file_dict = {}
+    for fname in dot_files:
+        temp = re.findall(r'\d+', fname) 
+        if temp:
+            file_dict[int(temp[0])] = fname
+
+    if not end:
+        end = len(file_dict)
+    assert end >= start
+    for i in range(end - start):
+        if i in file_dict:
+            dotf = join(path, file_dict[i])
+
+            graph = pydot.graph_from_dot_file(dotf)
+
+            s = Source.from_file(dotf)
+            s.view()
+            string = input("Press Enter to continue... (q+Enter to quit)")
+            if (string == "q"):
+                break
 
 
 def main():
-    # Placeholder input version
-    x = tf.placeholder(tf.float32, shape=xshape)
-    y = tf.placeholder(tf.int32, shape=yshape)
-    w = tf.get_variable("w", initializer=tf.ones(xshape), use_resource=True)
-
-    x = x * w  # In order to get global var init to compile
-
-    _standard_compile(model_fn, x, y)
-
-    (loss,) = generic_compile(model_fn, inputs=[x, y])
-
-    from tensorflow.tools.xla_extract import XlaExtract
-
-    # Dump XLA, set environment flag: XLA_FLAGS="--xla_hlo_graph_path=./ --xla_generate_hlo_graph=.*"
-
-    #train_op
-
     with tf.device("/job:localhost/replica:0/task:0/device:XLA_CPU:0"):
-        hlo_mod = XlaExtract(loss)
+        # Placeholder input version
+        x = tf.placeholder(tf.float32, shape=xshape)
+        y = tf.placeholder(tf.int32, shape=yshape)
+        #w = tf.get_variable("w", initializer=tf.ones(xshape), use_resource=True)
 
-    hlo_mod_string = hlo_mod.SerializeToString()
+        #x = x * w  # In order to get global var init to compile
 
-    with open("xla_out.proto", 'w') as f:
-        f.write(str(hlo_mod))
+        dot_dir = "./tmp_dot"
+        if os.path.exists(dot_dir):
+            shutil.rmtree(dot_dir)
+        os.mkdir(dot_dir)
+        
+        _standard_compile(model_fn, x, y)
+        _show_files(dot_dir, 0, 5)
 
-    with open("xla_out.bin", 'w') as f:
-        f.write(str(hlo_mod_string))
+        (loss,) = generic_compile(model_fn, inputs=[x, y])
 
-    def _draw_graph(logdir):
-        with tf.Session() as sess:
-            writer = tf.summary.FileWriter(logdir, sess.graph)
+        from tensorflow.tools.xla_extract import XlaExtract
 
-    # https://github.com/tensorflow/tensorflow/commit/39587aaeb7760c6c963d8668bd6f614360e914f8#diff-4b1165fb6148e99a9fb6fed2dd8979a6R189
+        # Dump XLA, set environment flag: XLA_FLAGS="--xla_hlo_graph_path=./ --xla_generate_hlo_graph=.*"
 
-    # XLA dump: https://groups.google.com/forum/#!topic/xla-dev/nMzNZfk-Jhw
+        #train_op
 
-    TB_LOGDIR = "/tmp/tblogdir"
-    if os.path.exists(TB_LOGDIR):
-        shutil.rmtree(TB_LOGDIR)
-        os.mkdir(TB_LOGDIR, mode=0o777)
+        with tf.device("/job:localhost/replica:0/task:0/device:XLA_CPU:0"):
+            hlo_mod = XlaExtract(loss)
 
-    _draw_graph(TB_LOGDIR)
+        hlo_mod_string = hlo_mod.SerializeToString()
+
+        with open("xla_out.proto", 'w') as f:
+            f.write(str(hlo_mod))
+
+        with open("xla_out.bin", 'w') as f:
+            f.write(str(hlo_mod_string))
+
+        def _draw_graph(logdir):
+            with tf.Session() as sess:
+                writer = tf.summary.FileWriter(logdir, sess.graph)
+
+        # https://github.com/tensorflow/tensorflow/commit/39587aaeb7760c6c963d8668bd6f614360e914f8#diff-4b1165fb6148e99a9fb6fed2dd8979a6R189
+
+        # XLA dump: https://groups.google.com/forum/#!topic/xla-dev/nMzNZfk-Jhw
+
+        TB_LOGDIR = "/tmp/tblogdir"
+        if os.path.exists(TB_LOGDIR):
+            shutil.rmtree(TB_LOGDIR)
+            os.mkdir(TB_LOGDIR, mode=0o777)
+
+        _draw_graph(TB_LOGDIR)
 
 main()
