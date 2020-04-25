493e3f68ff3d6e5ab892e5c647db1213d3b41f39
Chris Olivier <chris.olivier@cerebras.net>
2020-04-25 14:51:16 -0700

    Converting XLA compile to TF 2.0.1

--- tensorflow/tools/xla_extract/__init__.py
+++ tensorflow/tools/xla_extract/__init__.py
@@ -2,10 +2,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
  # pylint: disable=unused-import,wildcard-import, line-too-long
-from tensorflow.core.framework import graph_pb2
 from tensorflow.compiler.xla.service import hlo_pb2
 from tensorflow.python.framework import errors
-from tensorflow.python.pywrap_tensorflow import ExtractXlaWithStringInputs
+from tensorflow.python._pywrap_xla_extract import ExtractXlaWithStringInputs
 from tensorflow.python.util import compat
 
 
@@ -18,11 +17,15 @@ def XlaExtract(target_op):
     """
     targets_string = compat.as_bytes(target_op.name)
     graph_def_string = target_op.graph.as_graph_def(
-        add_shapes=True).SerializeToString()
+        add_shapes=True
+    ).SerializeToString()
 
     with errors.raise_exception_on_not_ok_status() as status:
         hlo_mod_string = ExtractXlaWithStringInputs(
-            graph_def_string, targets_string, status)
+            graph_def_string,
+            targets_string,
+            status,
+        )
         hlo_snapshot_def = hlo_pb2.HloModuleProto()
         hlo_snapshot_def.ParseFromString(hlo_mod_string)
         return hlo_snapshot_def
