3b58b7c2b59fe823105a1c346a88c49d870aec5d
Chris Olivier <chris.olivier@cerebras.net>
2020-05-01 18:58:43 +0000

    Add pytorch xla file directly rather than symlinks

--- tensorflow/compiler/xla/xla_client/metrics_reader.h
+++ tensorflow/compiler/xla/xla_client/metrics_reader.h
@@ -1,1 +1,15 @@
-../../../../../xla_client/metrics_reader.h
+#ifndef XLA_CLIENT_METRICS_READER_H_
+#define XLA_CLIENT_METRICS_READER_H_
+
+#include <string>
+
+namespace xla {
+namespace metrics_reader {
+
+// Creates a report with the current metrics statistics.
+std::string CreateMetricReport();
+
+}  // namespace metrics_reader
+}  // namespace xla
+
+#endif  // XLA_CLIENT_METRICS_READER_H_
